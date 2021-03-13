729bacc9709b6787f60adc666038279793792a84
Tom Camp <tom.camp@civicactions.com>
2021-03-13 09:01:55 -0700

    Basic setup.

---
+++ tests/test_compflask.py
@@ -0,0 +1,5 @@
+from compflask import __version__
+
+
+def test_version():
+    assert __version__ == '0.1.0'
---
+++ tests/test_comptools.py
@@ -0,0 +1,5 @@
+from comptools import __version__
+
+
+def test_version():
+    assert __version__ == '0.1.0'
